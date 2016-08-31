07dd80155736adf1c97e3ac51b10fa2a778d9fb7
Sondre Russvoll <sondre@russvoll.no>
2016-08-31 08:59:44 +0200

    .

---
+++ lib/ex1/ex1.cpp
@@ -0,0 +1,6 @@
+// Created by Sondre Vincent Russvoll on 31.08.16.
+// Copyright (c) 2016  All rights reserved.
+
+int ex1(void) {
+
+}
---
+++ lib/ex1/ex1.h
@@ -0,0 +1,9 @@
+// Created by Sondre Vincent Russvoll on 31.08.16.
+// Copyright (c) 2016  All rights reserved.
+
+#ifndef BYGGERN_EX1_H
+#define BYGGERN_EX1_H
+
+int ex1(void);
+
+#endif //BYGGERN_EX1_H

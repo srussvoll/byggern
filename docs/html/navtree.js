72c92c4b985febcc43fa78421b839a15b3adc4c3
Sondre Russvoll <sondre@russvoll.no>
2016-09-05 19:51:35 +0200

    Fixed DOXYGEN running on wrong source folder.

--- docs/html/navtree.js
+++ docs/html/navtree.js
@@ -1,11 +1,20 @@
 var NAVTREE =
 [
-  [ "Byggern", "index.html", ]
+  [ "Byggern", "index.html", [
+    [ "Byggern", "md__r_e_a_d_m_e.html", null ],
+    [ "Classes", null, [
+      [ "Class List", "annotated.html", "annotated" ],
+      [ "Class Index", "classes.html", null ]
+    ] ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ]
+    ] ]
+  ] ]
 ];
 
 var NAVTREEINDEX =
 [
-"index.html"
+"_u_a_r_t_8h_source.html"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
--- docs/html/search/search.js
+++ docs/html/search/search.js
@@ -7,10 +7,16 @@
 
 var indexSectionsWithContent =
 {
+  0: "bu",
+  1: "u",
+  2: "b"
 };
 
 var indexSectionNames =
 {
+  0: "all",
+  1: "classes",
+  2: "pages"
 };
 
 function convertToId(search)

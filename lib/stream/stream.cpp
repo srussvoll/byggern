b1c6167f488a5fd3ed646879ec5dc8d52f61ed23
unknown <johanhl@ITK-D651.win.ntnu.no>
2016-09-07 13:24:27 +0200

    Current progress with Stream lib

---
+++ lib/stream/stream.cpp
@@ -0,0 +1,46 @@
+#include "stream.h"
+namespace{
+	inline uint16_t min(uint16_t one, uint16_t two){
+		return (one >= two) ? one : two;
+	}
+}
+
+void Stream::Write(uint8_t *string, uint16_t size) {
+	if (size > this->GetAvailableWriteBytes()) {
+		this->output_stream_overflowed = true;
+	}
+	
+	// Either the write is on one side of index 0, or on both sides:
+	if(this->output_stream_stop_index + 1 + size > this->output_stream_size) {
+		// One sided write
+		memcpy(this->output_stream + this->output_stream_stop_index + 1, string, size);
+	} else {
+		// Two sided write
+		uint16_t size_one = this->output_stream_size - (this->output_stream_stop_index + 1);
+		uint16_t size_two = size - size_one;
+		memcpy(this->output_stream + this->output_stream_stop_index + 1, string, size_one);
+		memcpy(this->output_stream, string + size_one, size_two);
+	}
+	
+	this->output_stream_stop_index = (this->output_stream_stop_index + size) % this->output_stream_size;
+}
+
+
+
+void Stream::Read(uint8_t *string, size_t size) {
+	uint16_t read_size = min(size, this->GetAvailableReadBytes());
+	
+	// Either the read is on one side of index 0, or on both sides:
+    if (this->input_stream_start_index + read_size > this->input_stream_size) {
+		// One sided read
+		memcpy(string, this->input_stream + this->input_stream_start_index, read_size);
+	} else {
+		// Two sided read
+		uint16_t read_size_one = this->input_stream_size - this->input_stream_start_index;
+		uint16_t read_size_two = read_size - read_size_one;
+		memcpy(string, this->input_stream + this->input_stream_start_index, read_size_one);
+		memcpy(string + read_size_one, this->input_stream, read_size_two);
+	}
+	
+	this->input_stream_start_index = (this->input_stream_start_index + read_size) % this->input_stream_size;
+}
--- lib/stream/stream.h
+++ lib/stream/stream.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string.h>
+#include <avr/io.h>
 /**
  * @file
  * @author  Johan Lofstad, Sondre Baugstø, Sondre Russvoll
@@ -8,21 +9,98 @@
  *
  * An interface for handling streams with default methods.
  */
-class Stream {
+class Stream {	
+protected:
+
+	/**
+	 * Stores the input stream data
+	 */
+	uint8_t *input_stream = nullptr;
+	
+	/**
+	 * Stores the output stream data
+	 */
+	uint8_t *output_stream = nullptr;
+	
+	/**
+	 * An index that indicates where in the output stream the next bit is to be written
+	 */
+	uint16_t input_stream_start_index = 0;
+	
+	/**
+	 * An index that indicates where in the input stream the next bit is to be written
+	 */
+	uint16_t output_stream_start_index = 0;
+	
+	/**
+     * The size of the current output stream. This has to be set
+	 */
+	uint8_t output_stream_size = 0;
+	
+	/**
+	 * The size of the current input stream. This has to be set
+	 */
+	uint8_t input_stream_size = 0;
+	
+	/**
+	* The index indicates where the input ring buffer stops having data
+	*/
+	uint8_t input_stream_stop_index = 0;
+	
+	/**
+	 * The index indicates where the output ring buffer stops having data
+	 */
+	uint8_t output_stream_stop_index = 0;
+	
+	/**
+	 * Flag indicating whether the input stream has overflowed or not.
+	 */
+	bool input_stream_overflowed = false;
+	
+	/**
+	 * Flag indicating whether the output stream has overflowed or not.
+	 */
+	bool output_stream_overflowed = false;
 	
 public:
 	/**
-	* Writes the specified data to the output stream.
-	* @param string Input data
-	* @param size Size of the input data
-	*/
-	virtual void Write(char* string, size_t size);
+	 * The constructor. Needed to initialize stream sizes
+	 *
+	 */
+	Stream(uint8_t input_stream_size, uint8_t output_stream_size) : input_stream_size(input_stream_size),
+																	output_stream_size(output_stream_size){};
+
 	/**
-	* Reads data from the input stream and stores in the specified data.
-	* @param string Where the data should be stored
-	* @param size Size of the data
-	*/
-	virtual bool Read(char* string, size_t size);
+	 * Writes the specified data to the output stream.
+	 * @param string Input data
+	 * @param size Size of the input data
+	 */
+	virtual void Write(uint8_t *string, uint16_t size);
+	
+	/**
+	 * Reads data from the input stream and stores in the specified data.
+	 * @param string Where the data should be stored
+	 * @param size Size of the data
+	 */
+	virtual void Read(uint8_t *string, size_t size);
+	
+	/**
+	 * Simply returns the number of bytes available in the output buffer. The output depends on the maximum bytes allocated in the implementation.
+	 */
 	virtual uint8_t GetAvailableWriteBytes();
-	virtual uint8_t GetAvaliableReadBytes();
+	
+	/**
+	 * Simply returns the number of bytes available for reading (the actual data available for receiving in the buffer)
+	 */
+	virtual uint8_t GetAvailableReadBytes();
+	
+	/**
+	 * Reads one byte from the input stream
+	 */
+	virtual uint8_t ReadByte();
+		
+	/**
+	 * Writes one byte to the output stream
+	 */
+	virtual void WriteByte(uint8_t byte);
 };

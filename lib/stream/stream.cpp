c6c4843b269b170c8e7ac7ed05d5e6c9058b56d9
unknown <johanhl@ITK-D651.win.ntnu.no>
2016-09-07 20:38:25 +0200

    Further work on the stream

--- lib/stream/stream.cpp
+++ lib/stream/stream.cpp
@@ -27,24 +27,103 @@ void Stream::Write(uint8_t *string, uint16_t size) {
 
 
 
-void Stream::Read(uint8_t *string, size_t size) {
-	uint16_t read_size = min(size, this->GetAvailableReadBytes());
+void Stream::Read(uint8_t *string, uint16_t size) {
+	this->ReadFromBuffer(this->input_stream, this->output_stream_start_index, this->output_stream_stop_index, this->output_stream_size, string, size);
+}
+
+uint8_t Stream::GetAvailableWriteBytes(){
 	
-	// Either the read is on one side of index 0, or on both sides:
-    if (this->input_stream_start_index + read_size > this->input_stream_size) {
-		// One sided read
-		memcpy(string, this->input_stream + this->input_stream_start_index, read_size);
-	} else {
-		// Two sided read
-		uint16_t read_size_one = this->input_stream_size - this->input_stream_start_index;
-		uint16_t read_size_two = read_size - read_size_one;
-		memcpy(string, this->input_stream + this->input_stream_start_index, read_size_one);
-		memcpy(string + read_size_one, this->input_stream, read_size_two);
-	}
+}
+
+uint8_t Stream::GetAvailableReadBytes(){
 	
-	this->input_stream_start_index = (this->input_stream_start_index + read_size) % this->input_stream_size;
+}
+
+uint8_t Stream::ReadByte(){
+	
+}
+
+void Stream::WriteByte(uint8_t byte){
+	
 }
 
 bool Stream::CheckOutputOverflowFlag(){
 	
+}
+
+void Stream::ReadFromBuffer(uint8_t *buffer, uint16_t &start_index, uint16_t &stop_index, uint16_t &size, uint8_t *string, uint16_t &length){
+	uint16_t read_size = min(size, stop_index - start_index);
+	
+	// Either the read is on one side of index 0, or on both sides:
+	if (start_index + read_size > size) {
+		// One sided read
+		memcpy(string, buffer + start_index, read_size);
+		} else {
+		// Two sided read
+		uint16_t read_size_one = size - start_index;
+		uint16_t read_size_two = read_size - read_size_one;
+		memcpy(string, buffer + start_index, read_size_one);
+		memcpy(string + read_size_one, buffer, read_size_two);
+	}
+	
+	start_index = (start_index + read_size) % size;
+}
+
+void Stream::WriteToBuffer(uint8_t *buffer, uint16_t &start_index, uint16_t &stop_index, uint16_t &size, uint8_t *string, bool &overflow_flag, uint16_t &length){
+	if (length > (stop_index - start_index)) {
+		overflow_flag = true;
+	}
+	
+	// Either the write is on one side of index 0, or on both sides:
+	if(stop_index + 1 + length > size) {
+		// One sided write
+		memcpy(buffer + stop_index + 1, string, length);
+		} else {
+		// Two sided write
+		uint16_t length_one = size - (stop_index + 1);
+		uint16_t length_two = length - length_one;
+		memcpy(buffer + stop_index + 1, string, length_one);
+		memcpy(buffer, string + length_one, length_two);
+	}
+	
+	stop_index = (stop_index + length) % size;
+}
+
+void Stream::WriteByteToInputStream(uint8_t byte){
+	
+}
+
+uint8_t Stream::ReadByteFromOutputStream(){
+	
+}
+
+uint8_t Stream::ReadByteFromBuffer(uint8_t *buffer, uint16_t &start_index, uint16_t &stop_index, uint16_t &size){
+	
+}
+
+void Stream::WriteByteToBuffer(uint8_t *buffer, uint16_t &start_index, uint16_t &stop_index, uint16_t &size, uint8_t &byte, bool &overflow_flag){
+	
+	if(stop_index + 1 == size){ 
+		buffer[0] = byte;
+	}
+}
+
+uint16_t Stream::CalculateLength(uint16_t &start_index, uint16_t &stop_index){
+	
+}
+
+void Stream::UpdateStartIndex(uint16_t &start_index, uint16_t &stop_index, uint16_t &bytes_read){
+	if(((uint32_t)start_index + (uint32_t)bytes_read) >= 0xFFFF){
+		start_index %= 0x10000;
+		stop_index %= 0x10000;
+	}
+	start_index += bytes_read;
+}
+
+void Stream::UpdateStopIndex(uint16_t &start_index, uint16_t &stop_index, uint16_t &bytes_written){
+	if(((uint32_t)stop_index + (uint32_t)bytes_written) >= 0xFFFF){
+		start_index %= 0x10000;
+		stop_index %= 0x10000;
+	}
+	stop_index += bytes_written;	
 }
--- lib/stream/stream.h
+++ lib/stream/stream.h
@@ -35,22 +35,22 @@ protected:
 	/**
      * The size of the current output stream. This has to be set
 	 */
-	uint8_t output_stream_size = 0;
+	uint16_t output_stream_size = 0;
 	
 	/**
 	 * The size of the current input stream. This has to be set
 	 */
-	uint8_t input_stream_size = 0;
+	uint16_t input_stream_size = 0;
 	
 	/**
 	* The index indicates where the input ring buffer stops having data
 	*/
-	uint8_t input_stream_stop_index = 0;
+	uint16_t input_stream_stop_index = 0;
 	
 	/**
 	 * The index indicates where the output ring buffer stops having data
 	 */
-	uint8_t output_stream_stop_index = 0;
+	uint16_t output_stream_stop_index = 0;
 	
 	/**
 	 * Flag indicating whether the input stream has overflowed or not.
@@ -61,6 +61,84 @@ protected:
 	 * Flag indicating whether the output stream has overflowed or not.
 	 */
 	bool output_stream_overflowed = false;
+	
+	
+	/**
+	* Reads a string from the given buffer. 
+	* @param buffer Buffer to read from
+	* @param start_index The first valid byte of the buffer
+	* @param stop_index The last valid byte of the buffer
+	* @param size The size of the buffer
+	* @param string The string to read into
+	* @param length The length of the string
+	*/
+	void ReadFromBuffer(uint8_t *buffer, uint16_t &start_index, uint16_t &stop_index, uint16_t &size, uint8_t *string, uint16_t &length);
+	/**
+	* Writes a string to the given buffer. 
+	* @param buffer Buffer to write to
+	* @param start_index The first valid byte of the buffer
+	* @param stop_index The last valid byte of the buffer
+	* @param size The size of the buffer
+	* @param string The string to read from
+	* @param length The length of the string
+	*/
+	void WriteToBuffer(uint8_t *buffer, uint16_t &start_index, uint16_t &stop_index, uint16_t &size, uint8_t *string, bool &overflow_flag, uint16_t &length);
+	
+	/**
+	* Writes a byte to the input stream
+	* @param byte The byte to be written
+	*/
+	void WriteByteToInputStream(uint8_t byte);
+	
+	/**
+	* Reads a byte from the output stream
+	*/
+	uint8_t ReadByteFromOutputStream();
+	
+	/**
+	* Reads a byte from the given buffer
+	* @param buffer The buffer to read from
+	* @param start_index The first valid bit of the buffer
+	* @param stop_index The last valid bit of the buffer
+	* @param size The size of the buffer
+	* @return The byte that was read
+	*/
+	uint8_t ReadByteFromBuffer(uint8_t *buffer, uint16_t &start_index, uint16_t &stop_index, uint16_t &size);
+	
+	/**
+	* Writes a byte to the buffer
+	* @param buffer The buffer to write to
+	* @param start_index The first valid bit of the buffer
+	* @param stop_index The last valid bit of the buffer
+	* @param size The size of the buffer
+	* @param byte The byte to be written
+	* @param overflow_flag A flag indicated an overflow
+	*/
+	void WriteByteToBuffer(uint8_t *buffer, uint16_t &start_index, uint16_t &stop_index, uint16_t &size, uint8_t &byte, bool &overflow_flag);
+	
+	/**
+	* Calculates the length of the readable part of the buffer
+	* @param start_index The start index of the buffer
+	* @param stop_index The stop index of the buffer
+	* @return Length of valid data
+	*/
+	uint16_t CalculateLength(uint16_t &start_index, uint16_t &stop_index);
+	
+	/**
+	* Updates the start index
+	* @param start_index The start index of the buffer
+	* @param stop_index The stop index of the buffer
+	* @param bytes_read The amount of bytes already read
+	*/
+	void UpdateStartIndex(uint16_t &start_index, uint16_t &stop_index, uint16_t &bytes_read);
+	
+	/**
+	* Updates the stop index
+	* @param start_index The start index of the buffer
+	* @param stop_index The stop index of the buffer
+	* @param bytes_written The amount of bytes written already
+	*/
+	void UpdateStopIndex(uint16_t &start_index, uint16_t &stop_index, uint16_t &bytes_written);
 	
 public:
 	/**
@@ -82,7 +160,7 @@ public:
 	 * @param string Where the data should be stored
 	 * @param size Size of the data
 	 */
-	virtual void Read(uint8_t *string, size_t size);
+	virtual void Read(uint8_t *string, uint16_t size);
 	
 	/**
 	 * Simply returns the number of bytes available in the output buffer. The output depends on the maximum bytes allocated in the implementation.
@@ -108,4 +186,6 @@ public:
 	* Checks whether or not the output overflow flag has been set. If it has, it conducts the nessesary procedures to clear out the overflow
 	*/
 	virtual bool CheckOutputOverflowFlag();
+	
+
 };

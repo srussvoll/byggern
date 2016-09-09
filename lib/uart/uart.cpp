e28e48bef5ae8d540e089415699e484f3368e84d
sondrewbaugsto <sondrewbaugsto@gmail.com>
2016-09-09 22:10:09 +0200

    added basic uart options. to add: interrupts

--- lib/uart/uart.cpp
+++ lib/uart/uart.cpp
@@ -12,6 +12,7 @@ UART::UART(uint8_t baud_rate): Stream(64,64) {
 	UBRR0L = (uint8_t)(baud_rate >> 8);
 
 	// Sets the transmitter and receiver register
+	// If only receive set only TXEN0
 	UCSR0B = (1<<RXEN0)|(1<<TXEN0);
 	UCSR0C = (1<<URSEL0)|(1<<USBS0)|(3<<UCSZ00);
 
@@ -20,7 +21,9 @@ UART::UART(uint8_t baud_rate): Stream(64,64) {
 
 	// To enable printf
 
-	// Set the 
+	// Set the input_stream and output_stream pointers in Stream.
+	input_stream = input_stream_uart;
+	output_stream = output_stream_uart;
 
 	// fdevopen(&put, 0);
 
@@ -30,22 +33,10 @@ UART::UART(uint8_t baud_rate): Stream(64,64) {
 UART::Write(uint8_t *string, uint16_t size) {
 	Stream::Write(string, size);
 
-} 
-
-static int put(char character, FILE* file) {
-	send_data(character);
-	return 0;
-}
-
-uint8_t send_data(char data){
-	
-	
-	UDR0 = data;
-	while ((UCSR0A & (1 << UDRE0)) == 0) {
-		// do
+	// Check if the data register is ready to receive data
+	// If it is ready, write data to UDR0
+	if((UCSR0A & (1 << UDRE0)) == 1 ) {
+		UDR0 = ReadByteFromOutputStream();
 	}
-	
-	return 0;
-}
 
-
+} 
--- lib/uart/uart.h
+++ lib/uart/uart.h
@@ -22,17 +22,25 @@ public:
     */
     UART(uint8_t baud_rate);
 
+    /**
+    * Write the inserted string to output (i.e. write to computer)
+    * @param string The "data string" that shall be written to the output
+    * @param size the size of the data string
+    */
+    void Write(uint8_t *string, uint16_t size);
+
+
 private:
 
     /**
      * A 64 byte output stream. Everything that's sent from the microcontroller is first stored here.
      */
-    uint8_t output_stream[64];
+    uint8_t output_stream_uart[64];
 
     /**
      * A 64 byte input stream. Everything that's recieved from the client to the microcontroller is stored here.
      */
-    uint8_t input_stream[64];
+    uint8_t input_stream_uart[64];
 
     
 

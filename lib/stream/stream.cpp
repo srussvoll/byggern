789e0965ab8ba8cab7e9f3a048c1e6e6301c8271
Sondre Russvoll <sondre@russvoll.no>
2016-09-09 09:45:49 +0200

    Finished Stream

--- lib/stream/stream.cpp
+++ lib/stream/stream.cpp
@@ -1,129 +1,111 @@
 #include "stream.h"
 namespace{
-	inline uint16_t min(uint16_t one, uint16_t two){
-		return (one >= two) ? one : two;
-	}
+    inline uint16_t min(uint16_t one, uint16_t two){
+        return (one >= two) ? one : two;
+    }
 }
 
 void Stream::Write(uint8_t *string, uint16_t size) {
-	if (size > this->GetAvailableWriteBytes()) {
-		this->output_stream_overflowed = true;
-	}
-	
-	// Either the write is on one side of index 0, or on both sides:
-	if(this->output_stream_stop_index + 1 + size > this->output_stream_size) {
-		// One sided write
-		memcpy(this->output_stream + this->output_stream_stop_index + 1, string, size);
-	} else {
-		// Two sided write
-		uint16_t size_one = this->output_stream_size - (this->output_stream_stop_index + 1);
-		uint16_t size_two = size - size_one;
-		memcpy(this->output_stream + this->output_stream_stop_index + 1, string, size_one);
-		memcpy(this->output_stream, string + size_one, size_two);
-	}
-	
-	this->output_stream_stop_index = (this->output_stream_stop_index + size) % this->output_stream_size;
+	this->WriteToBuffer(this->output_stream, this->output_stream_start_index, this->output_stream_stop_index, this->output_stream_size, this->output_stream_overflowed, string, size);
 }
 
 
 
 void Stream::Read(uint8_t *string, uint16_t size) {
-	this->ReadFromBuffer(this->input_stream, this->output_stream_start_index, this->output_stream_stop_index, this->output_stream_size, string, size);
+    this->ReadFromBuffer(this->input_stream, this->output_stream_start_index, this->output_stream_stop_index, this->output_stream_size, string, size);
 }
 
 uint8_t Stream::GetAvailableWriteBytes(){
-	
+    return this->CalculateLength(this->output_stream_start_index, this->output_stream_stop_index, this->output_stream_size);
 }
 
 uint8_t Stream::GetAvailableReadBytes(){
-	
+    return this->CalculateLength(this->input_stream_start_index, this->input_stream_stop_index, this->input_stream_size);
 }
 
 uint8_t Stream::ReadByte(){
-	
+    return this->ReadByteFromBuffer(this->input_stream, this->input_stream_start_index, this->input_stream_stop_index, this->input_stream_size);
 }
 
 void Stream::WriteByte(uint8_t byte){
-	
+    this->WriteByteToBuffer(this->output_stream, this->output_stream_start_index, this->output_stream_stop_index, this->output_stream_size, this->output_stream_overflowed, byte);
 }
 
 bool Stream::CheckOutputOverflowFlag(){
-	
+    bool flag = this->output_stream_overflowed;
+	this->output_stream_overflowed = false;
+	return flag;
 }
 
-void Stream::ReadFromBuffer(uint8_t *buffer, uint16_t &start_index, uint16_t &stop_index, uint16_t &size, uint8_t *string, uint16_t &length){
-	uint16_t read_size = min(size, stop_index - start_index);
-	
-	// Either the read is on one side of index 0, or on both sides:
-	if (start_index + read_size > size) {
-		// One sided read
-		memcpy(string, buffer + start_index, read_size);
-		} else {
-		// Two sided read
-		uint16_t read_size_one = size - start_index;
-		uint16_t read_size_two = read_size - read_size_one;
-		memcpy(string, buffer + start_index, read_size_one);
-		memcpy(string + read_size_one, buffer, read_size_two);
-	}
-	
-	start_index = (start_index + read_size) % size;
+void Stream::ReadFromBuffer(uint8_t *buffer, uint16_t &start_index, uint16_t &stop_index, uint16_t &buffer_size, uint8_t *string, uint16_t &string_size){
+    uint16_t read_size = min(string_size, this->CalculateLength(start_index, stop_index, buffer_size));
+    
+    // Either the read is on one side of index 0, or on both sides:
+    if (start_index + read_size > buffer_size) {
+        // Two sided read
+        uint16_t read_size_one = buffer_size - start_index;
+        uint16_t read_size_two = read_size - read_size_one;
+        memcpy(string, buffer + start_index, read_size_one);
+        memcpy(string + read_size_one, buffer, read_size_two);
+    } else {
+        // One sided read
+        memcpy(string, buffer + start_index, read_size);
+    }
+    
+    start_index = (start_index + read_size) % buffer_size;
 }
 
-void Stream::WriteToBuffer(uint8_t *buffer, uint16_t &start_index, uint16_t &stop_index, uint16_t &size, uint8_t *string, bool &overflow_flag, uint16_t &length){
-	if (length > (stop_index - start_index)) {
-		overflow_flag = true;
-	}
-	
-	// Either the write is on one side of index 0, or on both sides:
-	if(stop_index + 1 + length > size) {
-		// One sided write
-		memcpy(buffer + stop_index + 1, string, length);
-		} else {
-		// Two sided write
-		uint16_t length_one = size - (stop_index + 1);
-		uint16_t length_two = length - length_one;
-		memcpy(buffer + stop_index + 1, string, length_one);
-		memcpy(buffer, string + length_one, length_two);
-	}
-	
-	stop_index = (stop_index + length) % size;
-}
-
-void Stream::WriteByteToInputStream(uint8_t byte){
-	
+void Stream::WriteToBuffer(uint8_t *buffer, uint16_t &start_index, uint16_t &stop_index, uint16_t &buffer_size, bool &overflow_flag, uint8_t *string, uint16_t &string_size){
+    if (string_size > buffer_size - this->CalculateLength(start_index, stop_index, buffer_size)) {
+        overflow_flag = true;
+    }
+    
+    // Either the write is on one side of index 0, or on both sides:
+    if(stop_index + 1 + string_size > buffer_size) {
+        // Two sided write
+        uint16_t size_one = buffer_size - (stop_index + 1);
+        uint16_t size_two = string_size - size_one;
+        memcpy(buffer + stop_index + 1, string, size_one);
+        memcpy(buffer, string + size_one, size_two);
+    } else {
+        // One sided write
+        memcpy(buffer + stop_index + 1, string, string_size);
+    }
+    
+    stop_index = (stop_index + string_size) % buffer_size;
 }
 
 uint8_t Stream::ReadByteFromOutputStream(){
-	
+    return this->ReadByteFromBuffer(this->output_stream, this->output_stream_start_index, this->output_stream_stop_index, this->output_stream_size);
 }
 
-uint8_t Stream::ReadByteFromBuffer(uint8_t *buffer, uint16_t &start_index, uint16_t &stop_index, uint16_t &size){
-	
+void Stream::WriteByteToInputStream(uint8_t byte){
+    this->WriteByteToBuffer(this->input_stream, this->input_stream_start_index, this->input_stream_stop_index, this->input_stream_size, this->input_stream_overflowed, byte);
 }
 
-void Stream::WriteByteToBuffer(uint8_t *buffer, uint16_t &start_index, uint16_t &stop_index, uint16_t &size, uint8_t &byte, bool &overflow_flag){
-	
-	if(stop_index + 1 == size){ 
-		buffer[0] = byte;
-	}
+uint8_t Stream::ReadByteFromBuffer(uint8_t *buffer, uint16_t &start_index, uint16_t &stop_index, uint16_t &buffer_size){
+    uint8_t value = buffer[start_index];
+    start_index = (start_index + 1) % buffer_size;
+    return value;
 }
 
-uint16_t Stream::CalculateLength(uint16_t &start_index, uint16_t &stop_index){
-	
+void Stream::WriteByteToBuffer(uint8_t *buffer, uint16_t &start_index, uint16_t &stop_index, uint16_t &buffer_size, bool &overflow_flag, uint8_t &byte) {
+    if (buffer_size - this->CalculateLength(start_index, stop_index, buffer_size) < 1) {
+        overflow_flag = true;
+    }
+
+    uint16_t to_write = (stop_index + 1) % buffer_size;
+    buffer[to_write] = byte;
+
+    stop_index = to_write;
 }
 
-void Stream::UpdateStartIndex(uint16_t &start_index, uint16_t &stop_index, uint16_t &bytes_read){
-	if(((uint32_t)start_index + (uint32_t)bytes_read) >= 0xFFFF){
-		start_index %= 0x10000;
-		stop_index %= 0x10000;
-	}
-	start_index += bytes_read;
+bool Stream::CheckInputOverflowFlag() {
+	bool flag = this->input_stream_overflowed;
+	this->input_stream_overflowed = false;
+	return flag;
 }
 
-void Stream::UpdateStopIndex(uint16_t &start_index, uint16_t &stop_index, uint16_t &bytes_written){
-	if(((uint32_t)stop_index + (uint32_t)bytes_written) >= 0xFFFF){
-		start_index %= 0x10000;
-		stop_index %= 0x10000;
-	}
-	stop_index += bytes_written;	
+uint16_t Stream::CalculateLength(uint16_t &start_index, uint16_t &stop_index, uint16_t &buffer_size) {
+    return (stop_index - start_index) % buffer_size;
 }

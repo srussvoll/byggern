c953969c6db14bb1f275a62127af8d708372cbfd
Gruppe 20 <gruppe20@itk-d650>
2016-09-21 10:25:01 +0200

    added fonts

---
+++ lib/oled/oled.h
@@ -0,0 +1,21 @@
+#include "lib/stream.h"
+
+/**
+ * @file
+ * @author  Johan Lofstad, Sondre Baugstø and Sondre Russvoll
+ * @version 1.0
+ *
+ * An interface to communicate with the oled display
+ */
+class OLED: Stream {
+public:
+    /**
+     * A Singleton implementation of this class
+     *
+     */
+    static OLED& GetInstance(){
+        static OLED instance;
+        return instance;
+    }
+
+};

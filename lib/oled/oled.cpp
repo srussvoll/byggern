84cd1f1a73dbba04e7713b7694927ae8fd6975c8
Gruppe 20 <gruppe20@itk-d650>
2016-09-23 09:31:49 +0200

    Sondre plez fix

--- lib/oled/oled.cpp
+++ lib/oled/oled.cpp
@@ -1,7 +1,31 @@
 #include "oled.h"
-
+#include <stdio.h>
 void OLED::Init(unsigned char c){
+    printf("Init oled");
+    *this->oled_command = 0xae; // display off
+    *this->oled_command = 0xa1; //segment remap
+    *this->oled_command = 0xda; //common pads hardware: alternative
+    *this->oled_command = 0x12;
+    *this->oled_command = 0xc8; //common output scan direction:com63~com0
+    *this->oled_command = 0xa8; //multiplex ration mode:63
+    *this->oled_command = 0x3f;
+    *this->oled_command = 0xd5; //display divide ratio/osc. freq. mode
+    *this->oled_command = 0x80;
+    *this->oled_command = 0x81; //contrast control
+    *this->oled_command = 0x50;
+    *this->oled_command = 0xd9; //set pre-charge period
+    *this->oled_command = 0x21;
+    *this->oled_command = 0x20; //Set Memory Addressing Mode
+    *this->oled_command = 0x02;
+    *this->oled_command = 0xdb; //VCOM deselect level mode
+    *this->oled_command = 0x30;
+    *this->oled_command = 0xad; //master configuration
+    *this->oled_command = 0x00;
+    *this->oled_command = 0xa4; //out follows RAM content
+    *this->oled_command = 0xa6; //set normal display
+    *this->oled_command = 0xaf; // display on
 
+    //this->event_output_buffer_not_empty = this->OutputBufferNotEmptyEvent;
 }
 
 void OLED::Clear(){
@@ -14,4 +38,11 @@ void OLED::ClearLine(){
 
 void OLED::GoToLine(uint8_t line){
     this->current_line = line;
+}
+
+void OLED::EventOutputBufferNotEmpty(){
+    while(this->GetOutputBufferLength() > 0){
+        // Write bytes to the oled data address
+
+    }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include "init.h"
 #include "lib/uart/uart.h"
+#include "lib/oled/oled.h"
 
 #include <stdlib.h>
 
@@ -54,35 +55,12 @@ int main(void) {
 
     printf("\n\n");
 
-    uint8_t *oled_command = (uint8_t*)0x3100;
-    uint8_t *oled_data    = (uint8_t*)0x3000;
+    OLED &my_oled = OLED::GetInstance();
 
     // INIT OLED
-    *oled_command = 0xae; // display off
-    *oled_command = 0xa1; //segment remap
-    *oled_command = 0xda; //common pads hardware: alternative
-    *oled_command = 0x12;
-    *oled_command = 0xc8; //common output scan direction:com63~com0
-    *oled_command = 0xa8; //multiplex ration mode:63
-    *oled_command = 0x3f;
-    *oled_command = 0xd5; //display divide ratio/osc. freq. mode
-    *oled_command = 0x80;
-    *oled_command = 0x81; //contrast control
-    *oled_command = 0x50;
-    *oled_command = 0xd9; //set pre-charge period
-    *oled_command = 0x21;
-    *oled_command = 0x20; //Set Memory Addressing Mode
-    *oled_command = 0x02;
-    *oled_command = 0xdb; //VCOM deselect level mode
-    *oled_command = 0x30;
-    *oled_command = 0xad; //master configuration
-    *oled_command = 0x00;
-    *oled_command = 0xa4; //out follows RAM content
-    *oled_command = 0xa6; //set normal display
-    *oled_command = 0xaf; // display on
-
+uint8_t *oled_data    = (uint8_t*)0x3100;
     while(true) {
-        *oled_data = 0x30AA;
+        *oled_data = 0xAA;
         _delay_us(500);
     }
 

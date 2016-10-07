17d4bb70d3ac4ff91dbb7597577d5b45ae2fb56d
johan <johan.lofstad@gmail.com>
2016-10-07 16:43:20 +0200

    More docs

--- lib/mcp2515/mcp2515.h
+++ lib/mcp2515/mcp2515.h
@@ -1,11 +1,16 @@
 #pragma once
 #include "../spi/spi.h"
+/**
+ * A singleton class which implements the communication between the AVR and the MCP2515. Throughout the documentation
+ * of this class, we will refer to the datasheet of the chip. This can be found at
+ * <a href="http://ww1.microchip.com/downloads/en/DeviceDoc/21801G.pdf">Microchips site</a>
+ */
 class MCP2515{
 public:
     /**
-        * A Singleton implementation of this class
-        *
-        */
+     * A Singleton implementation of this class
+     *
+     */
     static MCP2515& GetInstance(){
         static MCP2515 instance;
         return instance;
@@ -30,6 +35,9 @@ public:
 
 private:
     MCP2515(){};
+    /**
+     * Sends a RTS (Request to Send) signal to the MCP2515. This
+     */
     void RequestToSend();
     void WriteToRegister(uint8_t register_address, uint8_t byte);
     uint8_t ReadFromRegister(uint8_t register_address);

7af5dcb6d75cea210edb4236ba8083f0ea9060c9
gruppe20 <gruppe20@lab2>
2016-10-19 14:01:02 +0200

    Servo works like a charm.

--- lib/servo/servo.cpp
+++ lib/servo/servo.cpp
@@ -9,19 +9,34 @@
 #include "servo.h"
 #include <avr/io.h>
 
+namespace {
+    uint16_t mod(int16_t a, int16_t b) {
+        if (b < 0) return mod(a, -b);
+        int16_t ret = a % b;
+        if (ret < 0) ret += b;
+        return (uint16_t) ret;
+    }
+}
+
 Servo::Servo(uint16_t min, uint16_t max) : min(min), max(max) {
     // Set pin to output
-    DDRB |= (1 << PINB7);
-    // Set non inverting mode, set fast PWM
-    TCCR0A |= (3 << COM0A0) | (7 << WGM00);
+    DDRB |= (1 << DDB5);
+    // Set non inverting mode
+    TCCR1A |= (2 << COM1A0);
+    // Set fast PWM
+    TCCR1B |= (1 << WGM13) | (1 << WGM12);
+    TCCR1A |= (1 << WGM11) | (0 << WGM10);
     // Set frequency
-    TCNT0 |= (4 << CS00);
+    TCCR1B |= (3 << CS10);
+    ICR1 |= 5000;
 
-    OCR0A = 0x88;
+    this->SetAngle(0);
 }
 
 void Servo::SetAngle(int16_t angle) {
-
+    float angleMultiplier = ::mod(angle + 180, 360) / 360.0;
+    float counterMultiplier = (this->min + angleMultiplier * (this->max - this->min)) / 20000;
+    OCR1A = (uint16_t) (counterMultiplier * 5000);
 }
 
 #endif
--- node2/main.cpp
+++ node2/main.cpp
@@ -1,9 +1,17 @@
 #include "lib/servo/servo.h"
+#include <avr/io.h>
+#include <util/delay.h>
 
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wmissing-noreturn"
 int main(){
     Servo servo(900, 2100);
-    while(true);
+
+    int16_t angle = 0;
+    while(true) {
+        _delay_ms(1000);
+        angle += 20;
+        servo.SetAngle(angle);
+    }
 }
 #pragma clang diagnostic pop

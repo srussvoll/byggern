05d4aa821e6c9987b998b09cd7b0d0821b620249
sondrewbaugsto <sondrewb@stud.ntnu.no>
2016-10-26 19:59:45 +0000

    removed idea

--- node2/src/main.cpp
+++ node2/src/main.cpp
@@ -1,10 +1,17 @@
+#include <util/delay.h>
+
 #include "../lib/servo/servo.h"
-#include "../lib/uart/uart.h"
 
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wmissing-noreturn"
 int main(){
     //Servo servo(900, 2100);
-    while(true);
+    DDRA |= 1 << DDA7;
+    while(true){
+        PORTA |= 1 << PORTA7;
+        _delay_ms(1000);
+        PORTA |= 0 << PORTA7;
+        _delay_ms(1000);
+    }
 }
 #pragma clang diagnostic pop

5da411698a85d6db57047bd04cecd7a0b1d02e7d
sondrewbaugsto <sondrewb@stud.ntnu.no>
2016-10-26 13:42:12 +0000

    Changed structure and changed CMake

--- node2/main.cpp
+++ node2/main.cpp
@@ -3,7 +3,7 @@
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wmissing-noreturn"
 int main(){
-    Servo servo(900, 2100);
+    //Servo servo(900, 2100);
     while(true);
 }
 #pragma clang diagnostic pop

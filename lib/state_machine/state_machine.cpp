20d794bb11639faebec2a0c620955275dda190ed
gruppe20 <gruppe20@lab>
2016-10-26 22:01:44 +0200

    Finished state machine framework.

--- lib/state_machine/state_machine.cpp
+++ lib/state_machine/state_machine.cpp
@@ -1,5 +1,16 @@
 #include "state_machine.h"
 
-namespace StateMachine {
+bool StateMachine::Transition(uint8_t state, bool reenter = 0) {
+    if (state != this->current_state || state == this->current_state && reenter) {
+        uint8_t old_state = this->current_state;
+        this->current_state = state;
 
+        (*(this->transitions[(2 * old_state           + 1) * sizeof(this->transitions[0])]))();
+        (*(this->transitions[(2 * this->current_state + 0) * sizeof(this->transitions[0])]))();
+        return true;
+    } else return false;
+}
+
+uint8_t StateMachine::GetCurrentState() {
+    return this->current_state;
 }

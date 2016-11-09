c527b5248dce96ef7997f01e6f2c0187880d4325
gruppe20 <gruppe20@lab>
2016-11-09 11:44:06 +0100

    Fixed some bugs

--- node2/src/main.cpp
+++ node2/src/main.cpp
@@ -6,25 +6,18 @@
 #include <lib/oled_scp/oled_scp.h>
 #include "lib/spi/spi.h"
 #include "lib/mcp2515/mcp2515.h"
-<<<<<<< HEAD
 #include "lib/socket/socket.h"
 #include "lib/uart/uart.h"
 #include "lib/utilities/printf.h"
 #include "lib/scp/commands.h"
-=======
->>>>>>> 3c69ff9708ce95ac72e3f3df5308662406fb12f7
 #include <lib/dac/dac.h>
 #include <lib/ir_detector/ir_detector.h>
 #include "lib/socket/socket.h"
 #include "lib/uart/uart.h"
 #include "lib/utilities/printf.h"
-<<<<<<< HEAD
 #include "lib/adc_internal/adc_internal.h"
 #include "state_machine.h"
 #include "lib/timer/timer.h"
-=======
-#include "lib/scp/commands.h"
->>>>>>> 3c69ff9708ce95ac72e3f3df5308662406fb12f7
 
 void init_hardware_drivers() __attribute__((naked)) __attribute((used)) __attribute__((section(".init8")));
 void InitializeNetworkStack();
